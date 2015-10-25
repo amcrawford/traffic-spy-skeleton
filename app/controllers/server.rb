f24aabf3cef78612ce8030b50c6bc5e47c76c60b
Amber Crawford <ammcrawford@gmail.com>
2015-10-24 20:56:10 -0600

    Parser class

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -19,7 +19,6 @@ require 'uri'
     end
 
     post '/sources' do
-      # if valid parsed & not already in database
       source = Source.new(identifier: params[:identifier],
                           root_url: params[:rootUrl])
       if source.save
@@ -36,42 +35,10 @@ require 'uri'
     end
 
     post '/sources/:identifier/data' do
-      if params.include?("payload") && Source.exists?(:identifier => params["identifier"])
-        digest = Digest::SHA2.hexdigest(params.to_s)
-        source_id = Source.where(identifier: params["identifier"]).first.id
-        parsed = JSON.parse(params["payload"])
-        payload = Payload.new(url: parsed["url"],
-                              requested_at: parsed["requestedAt"],
-                              responded_in: parsed["respondedIn"],
-                              referred_by: parsed["referredBy"],
-                              request_type: parsed["requestType"],
-                              parameters: parsed["parameters"],
-                              event_name: parsed["eventName"],
-                              user_agent: parsed["userAgent"],
-                              resolution_width: parsed["resolutionWidth"],
-                              resolution_height: parsed["resolutionHeight"],
-                              ip: parsed["ip"],
-                              digest: digest,
-                              source_id: source_id)
-        if payload.save
-          status 200
-          body ""
-        elsif payload.errors.full_messages.join(", ")
-                                          .include?("already been taken")
-          status 403
-          body payload.errors.full_messages.join(", ")
-        else
-          status 400
-          body payload.errors.full_messages.join(", ")
-        end
-
-      elsif !Source.exists?(:identifier => params["identifier"])
-        status 403
-        body "Application not registered"
-      else
-        status 400
-        body "Missing Payload"
-      end
+      parser = Parser.new(params)
+      parser.post_payload
+      status parser.status
+      body parser.body
     end
 
     get '/sources/:identifier/urls/:relative' do
--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,4 +1,5 @@
 class Parser
+  attr_accessor :status, :body
   def initialize(params)
     @params = params
   end
@@ -22,4 +23,69 @@ class Parser
   def parsed_paths
     source.urls.map{ |payload| URI(payload.first).path } if source
   end
+
+  def validate_user_is_registered
+    Source.exists?(:identifier => @params["identifier"])
+  end
+
+  def validate_params_contain_payload
+    @params.include?("payload")
+  end
+
+  def post_payload
+    if validate_params_contain_payload && validate_user_is_registered
+      digest = Digest::SHA2.hexdigest(@params.to_s)
+      source_id = Source.where(identifier: @params["identifier"]).first.id
+      parsed = JSON.parse(@params["payload"])
+      payload = Payload.new(url: parsed["url"],
+                            requested_at: parsed["requestedAt"],
+                            responded_in: parsed["respondedIn"],
+                            referred_by: parsed["referredBy"],
+                            request_type: parsed["requestType"],
+                            parameters: parsed["parameters"],
+                            event_name: parsed["eventName"],
+                            user_agent: parsed["userAgent"],
+                            resolution_width: parsed["resolutionWidth"],
+                            resolution_height: parsed["resolutionHeight"],
+                            ip: parsed["ip"],
+                            digest: digest,
+                            source_id: source_id)
+      if payload.save
+        @status = 200
+        @body = ""
+      elsif payload.errors.full_messages.join(", ")
+        .include?("already been taken")
+        @status = 403
+        @body = payload.errors.full_messages.join(", ")
+      else
+        @status =  400
+        @body = payload.errors.full_messages.join(", ")
+      end
+
+    elsif !Source.exists?(:identifier => @params["identifier"])
+      @status = 403
+      @body = "Application not registered"
+    else
+      @status = 400
+      @body = "Missing Payload"
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
