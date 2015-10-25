c69d756a00971130df97b9512420edd4a74a9dce
Jason Pilz <jasonpilz@gmail.com>
2015-10-24 19:43:16 -0600

    refactoring feature tests yet some more

--- test/features/user_can_view_application_details_test.rb
+++ test/features/user_can_view_application_details_test.rb
@@ -10,39 +10,6 @@ class ViewDetailsTest < FeatureTest
     end
   end
 
-  # def setup
-  #   post '/sources', { "identifier" => "jumpstartlab",
-  #                      "rootUrl" => "http://jumpstartlab.com" }
-  #   post '/sources/jumpstartlab/data', {"payload"=> "{\"url\":\"http://jumpstartlab.com/blog\",
-  #                   \"requestedAt\":\"2013-02-16 21:38:28 -0700\",
-  #                   \"respondedIn\":37,
-  #                   \"referredBy\":\"http://jumpstartlab.com\",
-  #                   \"requestType\":\"GET\",
-  #                   \"parameters\":[],
-  #                   \"eventName\": \"socialLogin\",
-  #                   \"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",
-  #                   \"resolutionWidth\":\"1920\",
-  #                   \"resolutionHeight\":\"1280\",
-  #                   \"ip\":\"63.29.38.211\"}",
-  #                   "splat"=>[],
-  #                   "captures"=>["jumpstartlab"],
-  #                   "identifier"=>"jumpstartlab"}
-  #   post '/sources/jumpstartlab/data', {"payload"=> "{\"url\":\"http://jumpstartlab.com/team\",
-  #                   \"requestedAt\":\"2013-02-16 21:38:28 -0700\",
-  #                   \"respondedIn\":37,
-  #                   \"referredBy\":\"http://jumpstartlab.com\",
-  #                   \"requestType\":\"GET\",
-  #                   \"parameters\":[],
-  #                   \"eventName\": \"socialLogin\",
-  #                   \"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",
-  #                   \"resolutionWidth\":\"1920\",
-  #                   \"resolutionHeight\":\"1280\",
-  #                   \"ip\":\"63.29.38.211\"}",
-  #                   "splat"=>[],
-  #                   "captures"=>["jumpstartlab"],
-  #                   "identifier"=>"jumpstartlab"}
-  # end
-
   def test_user_can_view_all_data_at_homepage
     visit '/sources/jumpstartlab'
     assert page.has_content?("Requested URLs")
