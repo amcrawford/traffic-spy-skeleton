f62eccd20fa88cb10c26c8eda9575e89dcf311fe
Jason Pilz <jasonpilz@gmail.com>
2015-10-24 20:34:02 -0600

    Added some feature tests

--- test/features/user_can_view_application_details_test.rb
+++ test/features/user_can_view_application_details_test.rb
@@ -13,29 +13,28 @@ class ViewDetailsTest < FeatureTest
   def test_user_can_view_all_data_at_homepage
     visit '/sources/jumpstartlab'
     assert page.has_content?("Requested URLs")
-    assert page.has_content?("http://jumpstartlab.com/team: 1")
+    assert page.has_content?("http://jumpstartlab.com/index: 4")
 
     assert page.has_content?("Web Browser Breakdown")
-    assert page.has_content?("Chrome: 2")
+    assert page.has_content?("Chrome: 7")
 
     assert page.has_content?("OS Breakdown")
-    assert page.has_content?("OS X 10.8.2: 2")
+    assert page.has_content?("OS X 10.8.2")
 
     assert page.has_content?("Screen Resolution")
-    assert page.has_content?("1920 X 1280: 2")
+    assert page.has_content?("1920 X 800: 6")
 
     assert page.has_content?("Average URL Response Time")
-    assert page.has_content?("http://jumpstartlab.com/team: 37")
+    assert page.has_content?("http://jumpstartlab.com/home: 38")
 
     assert page.has_content?("Links to URL Stats")
-    assert page.has_content?("http://jumpstartlab.com/team")
+    assert page.has_content?("jumpstartlab/urls/home")
 
     assert page.has_content?("Links to Event Stats")
     assert page.has_content?("Event stats")
   end
 
   def test_registered_user_with_no_payload_sees_no_data
-    skip
     visit '/sources/jumpstartlab'
     assert page.has_content?("Requested URLs")
     assert page.has_content?("Web Browser Breakdown")
@@ -47,20 +46,17 @@ class ViewDetailsTest < FeatureTest
   end
 
   def test_unregistered_user_returns_error_message
-    skip
     visit '/sources/google'
     assert page.has_content? "identifier does not exist"
   end
 
   def test_url_links_redirect_to_individual_stat_pages
-    skip
     visit '/sources/jumpstartlab'
-    click_link_or_button("/blog_link")
-    assert_equal "/sources/jumpstartlab/urls/blog", current_path
+    click_link_or_button("/index_link")
+    assert_equal "/sources/jumpstartlab/urls/index", current_path
   end
 
   def test_url_links_redirect_to_event_stats_page
-    skip
     visit '/sources/jumpstartlab'
     click_link_or_button("event_link")
     assert_equal "/sources/jumpstartlab/events", current_path
--- test/features/user_can_view_events_index_test.rb
+++ test/features/user_can_view_events_index_test.rb
@@ -14,6 +14,14 @@ class ViewEventIndex < FeatureTest
   def test_user_can_view_index_of_events
     visit '/sources/jumpstartlab/events'
     assert page.has_content?("Event Index")
+    assert page.has_content?("socialLogin: 7")
+    assert_equal '/sources/jumpstartlab/events', current_path
   end
 
+  def test_event_links_to_event_details_page
+    visit '/sources/jumpstartlab/events'
+    assert_equal '/sources/jumpstartlab/events', current_path
+    click_link_or_button('socialLogin_link')
+    assert_equal '/sources/jumpstartlab/events/socialLogin', current_path
+  end
 end
